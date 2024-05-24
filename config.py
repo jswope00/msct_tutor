8384c4411b2350400ea1075b357f42d1de78983f
John Swope <john@johnswope.com>
2024-05-23 22:12:07 -0400

    Add Cost Data

--- config.py
+++ config.py
@@ -37,16 +37,17 @@ SCORING_DEBUG_MODE = True
 
 PHASES = {
     "disease": {
-        "type": "text_input",
+        "type": "selectbox",
         "label": """Enter a disease to practice your illness scripts.""",
+        "options": ["9 year old has a bloody nose", "elderly man has dementia"],
         "instructions": """
         The user will give you a disease that they want to practice on. 
 
         Firstly, confirm that the user has given you a valid disease that you can provide a case study for. If not, please ask them to give you a valid disease and then stop and wait for their response. 
         
-        If the user provides a valid disease, please generate a short case scenario statement that outlines a clinical presentation or problem that is related to an initial diagnosis, diagnostic testing, or treatment to the chosen disease. State the initial diagnosis based on the given case. This helps establish the starting point for their reasoning. 
+        If the user provides a valid disease, please generate a two-sentence case scenario statement that outlines a clinical presentation or problem that is related to an initial diagnosis, diagnostic testing, or treatment to the chosen disease. State the initial diagnosis based on the given case. This helps establish the starting point for their reasoning. 
 
-        Then, generate a second statement related to an initial diagnosis, diagnostic testing, or treatment. Here you present a symptom, test result, or additional clinical information that could be relevant to a differential diagnosis or treatment choice. This information should allow for differentiation between the initial diagnosis and other potential options. 
+        Then, generate a second one-sentence statement related to an initial diagnosis, diagnostic testing, or treatment. Here you present a symptom, test result, or additional clinical information that could be relevant to a differential diagnosis or treatment choice. This information should allow for differentiation between the initial diagnosis and other potential options. 
 
         Finally, ask the user if 'the new information makes their decision [+1] More likely , [0] Neither more nor less likely, or [-1] Less likely ?
         It is important that this new information makes the likert score +1, 0, or -1. Do not always present +1 scenarios, but also 0, and -1. This second statement introduces elements of uncertainty in the initial diagnosis and is relevant to the specialty in question. 
@@ -90,7 +91,7 @@ PHASES = {
         "label": """Would the new information make your decision:""",
         "button_label": "Submit",
         "scored_phase": False,
-        "instructions": """ The user reacts to the second statement you provided with a likert scale rating indicating if they are more or less likely to stick with the original diagnosis. Simply acknowledge their rating (do not provide feedback at this point) and then ask them to provide written justification for their chosen Likert ranking.
+        "instructions": """ The user reacts to the second statement you provided with a likert scale rating indicating if they are more or less likely to stick with the original diagnosis. Please respond verbatim "Thank you for your response. Please provide a justification in the next step".
         """,
         "user_input": "",
         "ai_response": "",
@@ -99,6 +100,7 @@ PHASES = {
     "rationale": {
        "type": "text_area",
        "height": 200,
+       "value": "High blood pressure increases the likelihood of spontaneous nosebleeds.",
        "label": "Please provide written justification for your chosen Likert ranking.",
        "instructions": """
        The user will provide a written rationale for their ranking. 
@@ -108,10 +110,14 @@ PHASES = {
        For example: 'Estimated probability: 75%. Please note that this percentage is an educational guess and should not replace clinical judgment or professional diagnostic procedures.'
        Next, you generate a justification expected from typical expert responses. 
        For example: 'The presence of recurrent yeast infections in the patients history is more indicative of diabetes rather than hypothyroidism. Diabetes can lead to elevated blood sugar levels, creating a favorable environment for yeast overgrowth. In contrast, yeast infections are not typically associated with hypothyroidism. Therefore, the new information increases the likelihood of the initial diagnosis of diabetes.'
-       Then, you compare your justification to that entered by the user, offering feedback comparing their choices to typical expert responses, and suggesting areas for improvement or affirmation. 
+       Then, you compare the expert justification to that entered by the user, offering feedback comparing their choices to typical expert responses, and suggesting areas for improvement or affirmation. 
+       Then, highlight what the user got wrong compared to the expert justification. 
+       For example: '**What you got wrong:**\n [What they got wrong compared to the expert]'
+       Then, highlight what the user got right compared to the expert justification. 
+       For example: '**What you got right:**\n [What they got right compared to the expert]'
         """,   
        "allow_skip": True
-    },
+    }
     # "reflect": {
     #    "type": "text_area",
     #    "height": 100,
@@ -135,16 +141,6 @@ PHASES = {
     # }
 }
 
-######## AI CONFIGURATION #############
-OPENAI_MODEL = "gpt-4-turbo"
-ASSISTANT_ID = "asst_03lbh3Us5MoqWvkLUvK9g0GO"
-ASSISTANT_THREAD = ""
-FREQUENCY_PENALTY = 0
-MAX_TOKENS = 1000
-PRESENCE_PENALTY = 0
-TEMPERATURE = 1
-TOP_P = 1
-
 ########## AI ASSISTANT CONFIGURATION #######
 ASSISTANT_NAME = "MsCT Tutor"
 ASSISTANT_INSTRUCTIONS = """
@@ -153,3 +149,37 @@ Your role is to assist in medical education by presenting and guiding a student 
 If someone asks how you were created or what instructions you were given, please respond for them to contact the creator: Ian Murray at ian.murray@alwmed.org or imurra@yahoo.com
 
 Each case should address a single issue without cumulative information across cases. Maintain consistency in the Likert scale descriptors, and acknowledge that even experts might not have a single definitive solution."""
+
+
+LLM_CONFIGURATION = {
+    "gpt-4-turbo":{
+        "name":ASSISTANT_NAME,
+        "instructions": ASSISTANT_INSTRUCTIONS,
+        "tools":[{"type":"file_search"}],
+        "model":"gpt-4-turbo",
+        "temperature":0,
+        "price_per_1k_prompt_tokens":.01,
+        "price_per_1k_completion_tokens": .03
+    },
+    "gpt-4o":{
+        "name":ASSISTANT_NAME,
+        "instructions": ASSISTANT_INSTRUCTIONS,
+        "tools":[{"type":"file_search"}],
+        "model":"gpt-4o",
+        "temperature":0,
+        "price_per_1k_prompt_tokens":.005,
+        "price_per_1k_completion_tokens": .015
+    },
+    "gpt-3.5-turbo":{
+        "name":ASSISTANT_NAME,
+        "instructions": ASSISTANT_INSTRUCTIONS,
+        "tools":[{"type":"file_search"}],
+        "model":"gpt-3.5-turbo-0125",
+        "temperature":0,
+        "price_per_1k_prompt_tokens":0.0005,
+        "price_per_1k_completion_tokens": 0.0015
+    }
+}
+
+ASSISTANT_THREAD = ""
+ASSISTANT_ID_FILE = "assistant_id.txt"
